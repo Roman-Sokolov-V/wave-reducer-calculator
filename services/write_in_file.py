3ea87a2d274be5af7eda190040f69c1a54f3b649
Roman Sokolov <gnonasis@gmail.com>
2025-12-21 19:55:35 +0200

    add write_in_file.py  ReducerFXFExporter

---
+++ services/write_in_file.py
@@ -0,0 +1,62 @@
+import ezdxf
+
+from reducer.geometry import ReducerGeometry
+
+
+class ReducerDXFExporter:
+    def __init__(
+            self,
+            geometry: ReducerGeometry
+    ):
+        self.stacked_xy = geometry.stacked_xy
+        self.outer_radius = geometry.outer_radius
+        self.inner_radius = geometry.inner_radius
+        self.radius_of_separator_outer = geometry.radius_of_separator_outer
+        self.radius_of_separator_inner = geometry.radius_of_separator_inner
+        self.eccentricity = geometry.eccentricity
+        self.eccentric_radius = geometry.eccentric_radius
+
+    def _write_to_file_base_wheel_shape(self):
+        doc = ezdxf.new("R2000")
+        msp = doc.modelspace()
+
+        msp.add_point([0, 0])
+        msp.add_spline(self.stacked_xy)
+        msp.add_circle((0, 0), radius=self.outer_radius)
+        msp.add_circle((0, 0), radius=self.inner_radius)
+        doc.saveas("base_wheel.dxf")
+
+
+    def _write_to_file_separator(self):
+        doc = ezdxf.new("R2000")
+        msp = doc.modelspace()
+
+        msp.add_circle((0, 0), radius=self.radius_of_separator_outer)
+        msp.add_circle((0, 0), radius=self.radius_of_separator_inner)
+        doc.saveas("separator.dxf")
+
+
+    def _write_to_file_eccentric(self):
+        doc = ezdxf.new("R2000")
+        msp = doc.modelspace()
+
+        msp.add_point([0, self.eccentricity])
+        msp.add_lwpolyline([[0, 0], [0, self.eccentricity]])
+        msp.add_lwpolyline([[-6, 0], [6, 0]])
+        msp.add_lwpolyline([[-3, self.eccentricity], [3, self.eccentricity]])
+        msp.add_circle((0, self.eccentricity), radius=self.eccentric_radius)
+        doc.saveas("eccentric.dxf")
+
+
+    def write_profiles_to_files(
+            self,
+            will_shape: bool = True,
+            separator: bool = True,
+            eccentric: bool = True
+    ):
+        if will_shape:
+            self._write_to_file_base_wheel_shape()
+        if separator:
+            self._write_to_file_separator()
+        if eccentric:
+            self._write_to_file_eccentric()
