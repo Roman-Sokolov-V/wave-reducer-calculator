24f98ddea64ae4306b39ae540d04cbcc46b2df2f
Roman Sokolov <gnonasis@gmail.com>
2025-12-28 14:24:56 +0200

    changed ui

--- ui/ui_main_window.py
+++ ui/ui_main_window.py
@@ -26,25 +26,17 @@ class Ui_MainWindow(object):
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(1019, 568)
         MainWindow.setMinimumSize(QSize(100, 100))
+        MainWindow.setStyleSheet(u"")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setStyleSheet(u"background-color: qlineargradient(\n"
-"    x1:0, y1:0, x2:0, y2:1,\n"
-"    stop:0.0   rgba(0,  170, 255, 255),   /* \u0447\u0438\u0441\u0442\u0438\u0439 \u0441\u0438\u043d\u0456\u0439 */\n"
-"    stop:0.4   rgba(135, 206, 250, 255),/* \u0431\u043b\u0430\u043a\u0438\u0442\u043d\u0438\u0439 */\n"
-"    stop:0.6   rgba(255, 255, 224, 255),/* \u0431\u043b\u0456\u0434\u043e-\u0436\u043e\u0432\u0442\u0438\u0439 */\n"
-"    stop:1.0   rgba(255, 255, 100, 255)   /* \u0447\u0438\u0441\u0442\u0438\u0439 \u0436\u043e\u0432\u0442\u0438\u0439 */\n"
-");\n"
-"")
+        self.centralwidget.setStyleSheet(u"")
         self.verticalLayout_7 = QVBoxLayout(self.centralwidget)
         self.verticalLayout_7.setObjectName(u"verticalLayout_7")
         self.all = QVBoxLayout()
         self.all.setObjectName(u"all")
         self.menu_frame = QFrame(self.centralwidget)
         self.menu_frame.setObjectName(u"menu_frame")
-        self.menu_frame.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));\n"
-"border: 1px solid rgba(255, 255, 255, 40);\n"
-"border-radius: 5px;")
+        self.menu_frame.setStyleSheet(u"")
         self.horizontalLayout_2 = QHBoxLayout(self.menu_frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout_11 = QHBoxLayout()
@@ -175,7 +167,7 @@ class Ui_MainWindow(object):
         self.resolution.setObjectName(u"resolution")
         sizePolicy.setHeightForWidth(self.resolution.sizePolicy().hasHeightForWidth())
         self.resolution.setSizePolicy(sizePolicy)
-        self.resolution.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
+        self.resolution.setStyleSheet(u"")
         self.resolution.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.PlusMinus)
 
         self.horizontalLayout_6.addWidget(self.resolution)
@@ -221,59 +213,43 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addWidget(self.label_16)
 
-        self.horizontalLayout_7 = QHBoxLayout()
+        self.widget = QWidget(self.menu_frame)
+        self.widget.setObjectName(u"widget")
+        self.widget.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
+        self.horizontalLayout_7 = QHBoxLayout(self.widget)
         self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
-        self.checkBox_track = QCheckBox(self.menu_frame)
+        self.checkBox_eccentric = QCheckBox(self.widget)
+        self.checkBox_eccentric.setObjectName(u"checkBox_eccentric")
+        self.checkBox_eccentric.setStyleSheet(u"")
+        self.checkBox_eccentric.setChecked(True)
+        self.checkBox_eccentric.setTristate(False)
+
+        self.horizontalLayout_7.addWidget(self.checkBox_eccentric)
+
+        self.checkBox_separator = QCheckBox(self.widget)
+        self.checkBox_separator.setObjectName(u"checkBox_separator")
+        self.checkBox_separator.setAutoFillBackground(False)
+        self.checkBox_separator.setStyleSheet(u"")
+        self.checkBox_separator.setChecked(True)
+
+        self.horizontalLayout_7.addWidget(self.checkBox_separator)
+
+        self.checkBox_balls = QCheckBox(self.widget)
+        self.checkBox_balls.setObjectName(u"checkBox_balls")
+        self.checkBox_balls.setStyleSheet(u"")
+        self.checkBox_balls.setChecked(True)
+
+        self.horizontalLayout_7.addWidget(self.checkBox_balls)
+
+        self.checkBox_track = QCheckBox(self.widget)
         self.checkBox_track.setObjectName(u"checkBox_track")
-        self.checkBox_track.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
+        self.checkBox_track.setChecked(True)
 
         self.horizontalLayout_7.addWidget(self.checkBox_track)
 
-        self.horizontalLayout_7.setStretch(0, 1)
 
-        self.verticalLayout_3.addLayout(self.horizontalLayout_7)
+        self.verticalLayout_3.addWidget(self.widget)
 
-        self.horizontalLayout_8 = QHBoxLayout()
-        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
-        self.checkBox_balls = QCheckBox(self.menu_frame)
-        self.checkBox_balls.setObjectName(u"checkBox_balls")
-        self.checkBox_balls.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
-
-        self.horizontalLayout_8.addWidget(self.checkBox_balls)
-
-        self.horizontalLayout_8.setStretch(0, 1)
-
-        self.verticalLayout_3.addLayout(self.horizontalLayout_8)
-
-        self.horizontalLayout_9 = QHBoxLayout()
-        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
-        self.checkBox_separator = QCheckBox(self.menu_frame)
-        self.checkBox_separator.setObjectName(u"checkBox_separator")
-        self.checkBox_separator.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
-
-        self.horizontalLayout_9.addWidget(self.checkBox_separator)
-
-        self.horizontalLayout_9.setStretch(0, 1)
-
-        self.verticalLayout_3.addLayout(self.horizontalLayout_9)
-
-        self.horizontalLayout_10 = QHBoxLayout()
-        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
-        self.checkBox_eccentric = QCheckBox(self.menu_frame)
-        self.checkBox_eccentric.setObjectName(u"checkBox_eccentric")
-        self.checkBox_eccentric.setStyleSheet(u"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 100), stop:1 rgba(255, 255, 255, 255));")
-
-        self.horizontalLayout_10.addWidget(self.checkBox_eccentric)
-
-        self.horizontalLayout_10.setStretch(0, 1)
-
-        self.verticalLayout_3.addLayout(self.horizontalLayout_10)
-
-        self.verticalLayout_3.setStretch(0, 1)
-        self.verticalLayout_3.setStretch(1, 1)
-        self.verticalLayout_3.setStretch(2, 1)
-        self.verticalLayout_3.setStretch(3, 1)
-        self.verticalLayout_3.setStretch(4, 1)
 
         self.verticalLayout_4.addLayout(self.verticalLayout_3)
 
@@ -288,13 +264,9 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_4.addWidget(self.export_pushButton)
 
-        self.verticalLayout_4.setStretch(0, 5)
-        self.verticalLayout_4.setStretch(1, 1)
 
         self.verticalLayout_5.addLayout(self.verticalLayout_4)
 
-        self.verticalLayout_5.setStretch(0, 4)
-        self.verticalLayout_5.setStretch(1, 3)
 
         self.horizontalLayout_11.addLayout(self.verticalLayout_5)
 
@@ -342,10 +314,10 @@ class Ui_MainWindow(object):
         self.label_9.setText(QCoreApplication.translate("MainWindow", u"resolution", None))
         self.visualize_pushButton.setText(QCoreApplication.translate("MainWindow", u"Generate & Visualize", None))
         self.label_16.setText(QCoreApplication.translate("MainWindow", u"Export Options", None))
-        self.checkBox_track.setText(QCoreApplication.translate("MainWindow", u"export track", None))
-        self.checkBox_balls.setText(QCoreApplication.translate("MainWindow", u"export balls", None))
-        self.checkBox_separator.setText(QCoreApplication.translate("MainWindow", u"export separator", None))
-        self.checkBox_eccentric.setText(QCoreApplication.translate("MainWindow", u"export eccentric", None))
+        self.checkBox_eccentric.setText(QCoreApplication.translate("MainWindow", u"eccentric", None))
+        self.checkBox_separator.setText(QCoreApplication.translate("MainWindow", u"separator", None))
+        self.checkBox_balls.setText(QCoreApplication.translate("MainWindow", u"balls", None))
+        self.checkBox_track.setText(QCoreApplication.translate("MainWindow", u"track", None))
         self.export_pushButton.setText(QCoreApplication.translate("MainWindow", u"Export to DXF", None))
         self.notifications.setText(QCoreApplication.translate("MainWindow", u"notifications", None))
     # retranslateUi
